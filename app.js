d7f12109e4e38d9de72a52d6364a252bb726a1ce
Victor <rixincui@gmail.com>
2016-01-21 10:39:28 +0800

    Add cross domain

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ var secure = require('./routes/secure');
 
 var app = express();
 
+// 设置头
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
